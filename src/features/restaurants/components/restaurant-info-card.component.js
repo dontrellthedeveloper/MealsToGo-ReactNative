9017aa46552238cb5701c6b0ac66854be31b2c43
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-03-26 21:24:20 -0700

    fixed android bug

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,73 +1,39 @@
 import React from "react";
-import { SvgXml } from "react-native-svg";
-import { View } from "react-native";
+import styled from "styled-components/native";
+import WebView from "react-native-webview";
+import { Platform } from "react-native";
 
-import { Favourite } from "../../../components/favourites/favourite.component";
-import { Spacer } from "../../../components/spacer/spacer.component";
-import { Text } from "../../../components/typography/text.component";
-import star from "../../../../assets/star";
-import open from "../../../../assets/open";
+import { Text } from "../typography/text.component";
 
-import {
-  RestaurantCard,
-  RestaurantCardCover,
-  Info,
-  Section,
-  SectionEnd,
-  Rating,
-  Icon,
-  Address,
-} from "./restaurant-info-card.styles";
+const CompactImage = styled.Image`
+  border-radius: 10px;
+  width: 120px;
+  height: 100px;
+`;
 
-export const RestaurantInfoCard = ({ restaurant = {} }) => {
-  const {
-    name = "Some Restaurant",
-    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
-    photos = [
-      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
-    ],
-    address = "100 some random street",
-    isOpenNow = true,
-    rating = 4,
-    isClosedTemporarily = true,
-    placeId,
-  } = restaurant;
+const CompactWebview = styled(WebView)`
+  border-radius: 10px;
+  width: 120px;
+  height: 100px;
+`;
 
-  const ratingArray = Array.from(new Array(Math.floor(rating)));
+const Item = styled.View`
+  padding: 10px;
+  max-width: 120px;
+  align-items: center;
+`;
+
+const isAndroid = Platform.OS === "android";
+
+export const CompactRestaurantInfo = ({ restaurant, isMap }) => {
+  const Image = isAndroid && isMap ? CompactWebview : CompactImage;
 
   return (
-    <RestaurantCard elevation={5}>
-      <View>
-        <Favourite restaurant={restaurant} />
-        <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-      </View>
-      <Info>
-        <Text variant="label">{name}</Text>
-        <Section>
-          <Rating>
-            {ratingArray.map((_, i) => (
-              <SvgXml
-                key={`star-${placeId}-${i}`}
-                xml={star}
-                width={20}
-                height={20}
-              />
-            ))}
-          </Rating>
-          <SectionEnd>
-            {isClosedTemporarily && (
-              <Text variant="error">CLOSED TEMPORARILY</Text>
-            )}
-            <Spacer position="left" size="large">
-              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
-            </Spacer>
-            <Spacer position="left" size="large">
-              <Icon source={{ uri: icon }} />
-            </Spacer>
-          </SectionEnd>
-        </Section>
-        <Address>{address}</Address>
-      </Info>
-    </RestaurantCard>
+    <Item>
+      <Image source={{ uri: restaurant.photos[0] }} />
+      <Text center variant="caption" numberOfLines={3}>
+        {restaurant.name}
+      </Text>
+    </Item>
   );
 };
