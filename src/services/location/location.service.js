9b40c2e757c16f7c25435df6c88ff43c5cf8580d
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-04-01 16:59:33 -0700

    integrated places API

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -2,13 +2,14 @@ import camelize from "camelize";
 import { host } from "../../utils/env";
 
 export const locationRequest = (searchTerm) => {
-  return fetch(`${host}/geocode?city=${searchTerm}&mock=true`).then((res) => {
+  return fetch(`${host}/geocode?city=${searchTerm}`).then((res) => {
     return res.json();
   });
 };
 
 export const locationTransform = (result) => {
   const formattedResponse = camelize(result);
+  console.log(result);
   const { geometry = {} } = formattedResponse.results[0];
   const { lat, lng } = geometry.location;
 
