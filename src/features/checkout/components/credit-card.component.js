4c4b69b15c0769f36a5c4d8673c6d221dfcc73dc
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-04-02 23:58:13 -0700

    added the ability to send basic stripe payment

--- src/features/checkout/components/credit-card.component.js
+++ src/features/checkout/components/credit-card.component.js
@@ -1,11 +1,24 @@
 import React from "react";
 import { LiteCreditCardInput } from "react-native-credit-card-input";
+import createStripe from "stripe-client";
 
+const stripe = createStripe(
+  "pk_test_51Ic2k0AEBlHURtd4wyfTGovhgVLJcmOJpyeaDWlDHtHHs6jpLt6h5EFNTD7fS9fZQiT9olP3m8ERMjKmaLHQPJ9V009UJn5WP5"
+);
 export const CreditCardInput = () => {
-  const onChange = (formData) => {
+  const onChange = async (formData) => {
     const { values, status } = formData;
     const isIncomplete = Object.values(status).includes("incomplete");
     console.log(isIncomplete);
+    const card = {
+      number: "4242424242424242",
+      exp_month: "02",
+      exp_year: "24",
+      cvc: "244",
+      name: "Dontrell",
+    };
+    const info = await stripe.createToken({ card });
+    console.log(info);
   };
   return <LiteCreditCardInput onChange={onChange} />;
 };
